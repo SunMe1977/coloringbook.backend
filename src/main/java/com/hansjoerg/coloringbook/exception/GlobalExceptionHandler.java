f7a95de12974dc2b3a100919834fb6aba0dbab10
hjh <hansjoerghofer692@gmail.com>
2025-09-09 16:56:27 +0200

    refactot yml

---
+++ src/main/java/com/hansjoerg/coloringbook/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.hansjoerg.coloringbook.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Something went wrong: " + ex.getMessage());
+    }
+
+    // Add more handlers as needed
+}
