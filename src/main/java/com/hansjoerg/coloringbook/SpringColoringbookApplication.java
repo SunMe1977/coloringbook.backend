bb2e6efefa3fa88a01481899a1ad72b7a4961a1d
hjh <hansjoerghofer692@gmail.com>
2025-09-12 10:41:57 +0200

    speedup start

--- src/main/java/com/hansjoerg/coloringbook/SpringColoringbookApplication.java
+++ src/main/java/com/hansjoerg/coloringbook/SpringColoringbookApplication.java
@@ -3,9 +3,14 @@ package com.hansjoerg.coloringbook;
 import com.hansjoerg.coloringbook.config.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        WebSocketServletAutoConfiguration.class,
+        SecurityAutoConfiguration.class
+})
 @EnableConfigurationProperties(AppProperties.class)
 public class SpringColoringbookApplication {
 
